f80fda9e36466da7e8075205843e37b639d8adde
Muhammad <amirbaba8@gmail.com>
2022-11-23 15:29:06 -0800

    Printf func

---
+++ _printf.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _printf - function that prints anything
+ * @format: list of types of arguments passed to the function
+ * Return: Returns number of printed characters if successful
+ * or -1 if error
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	int i, char_count, char_no;
+
+	int (*func_pointer)(va_list);
+
+	i = char_count = char_no = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				break;
+
+			func_pointer = is_special_character(&format[i + 1]);
+
+			if (func_pointer != NULL)
+			{
+				char_no = func_pointer(args);
+				char_count += char_no;
+				i += 2;
+			}
+		}
+
+		char_no = _putchar(format[i]);
+		char_count += char_no;
+		i++;
+	}
+	va_end(args);
+	return (char_count);
+}
+
