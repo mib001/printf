9706de05908f251c40ae9109748fa01a7c676449
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2022-11-26 00:34:13 +0100

    Updated pcent_fun.c file

---
+++ pcent_fun.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *pcen_fun - prints one %
+ * @args: variadic parameter
+ *
+ * Return: number of characters printed
+ */
+
+int pcent_fun(va_list args)
+{
+	(void)args;
+
+	_putchar('%');
+
+	return (1);
+}
