6757d0d5d07c902dd1e7b947602d6f1be7ce2bc0
mahmud.mahmud <mahmud.mahmudx00@gmail.com>
2022-11-25 22:55:18 +0100

    Create is_special_character.c

---
+++ is_special_character.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * is_special_character - check the argument that is  character passed is and
+ * compare it with the special character and
+ *then return suitable function to the paramete
+ *@format: the special character from the _prinf()
+ *
+ *Return: function pointer
+ */
+
+int (*is_special_character(const char *format))(va_list)
+{
+	int i = 0;
+
+	/*This an array of struct initialization */
+	func_t sc_array[6] = {
+		{"c", char_fun},
+		{"s", str_fun},
+		{"%", pcent_fun},
+		{"d", dec_fun},
+		{"i", int_fun},
+		{NULL, NULL}};
+
+	/*
+	*This will call the function suitable to the first index of array after
+	*checking it against format
+	 */
+	while (sc_array[i].t != NULL)
+	{
+		if (*(sc_array[i].t) == *format)
+			return (sc_array[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
